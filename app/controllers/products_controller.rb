a42f42681517b99b75b2aacda5ee2fd5a2efa4f0
Alex <alexgmiller00@gmail.com>
2022-03-14 10:21:43 -0700

    Adds gem rack-cors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,57 @@
 class ProductsController < ApplicationController
+    def index
+        products = Product.all
+        render json: products.as_json
+    end
+
+    def show
+        input = params["id"]
+        product = Product.find_by(id: input)
+        render json: (product)
+    end
+
+    def create
+        product = Product.new(
+            name: params["name"],
+            price: params["price"],
+            image_url: params["image_url"],
+            description: params["description"])
+        product.save
+    end
+
+    def update
+        product_id = params[:id]
+        product = Product.find_by(id: product_id)
+
+        product.name = params["name"] || product.name
+        product.price = params["price"] || product.price
+        product.image_url = params["image"] || product.image_url
+        product.description = params["description"] || product.description
+        product.save
+        render json: recipe.as_json
+    end
+
+    def destroy
+        product_id = params[:id]
+        product = Product.find_by(id: product_id)
+        product.destroy
+        render json: {message: "Product id: #{product_id} has been deleted."}
+    end
+
+
+    # def first_product
+    #     product = Product.first
+    #     render json: product.as_json
+    # end
+
+    # def second_product
+    #     product = Product.second
+    #     render json: product.as_json
+    # end
+
+    # def third_product
+    #     product = Product.third
+    #     render json: product.as_json
+    # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get "/all_products", controller: "products", action: "all_products"
+  # get "/all_products" => "products#all_products"
+  # get "/first_product" => "products#first_product"
+  # get "/second_product" => "products#second_product"
+  # get "/third_product" => "products#third_product"
+  # get "/product_search" => "products#product_search"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
