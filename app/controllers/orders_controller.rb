aead135cad1736442dc8a71e39c1e8e8f2d075be
Alex <alexgmiller00@gmail.com>
2022-03-22 12:13:45 -0700

    Adds admin

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,32 @@
 class OrdersController < ApplicationController
+    before_action :authenticate_user
+
     def create
-        orders = Order.new(
-            user_id: params["user_id"],
-            product_id: params["product_id"],
-            quantity: params["quantity"],
-            quantity: params["quantity"])
-            subtotal: params["subtotal"]
-            tax: params["tax"]
-            total: params["total"]
-        @orders.save
-        render json: @orders.as_json
+        product = Product.find_by(id: params[:product_id])
+        calclated_subtotal = product.price * params[:quantity]
+        calculated_tax = calculated_subtotal * 0.09
+        calculated_total = calculated_subtotal + calculated_tax
+
+        order = Order.new(
+            user_id: current_user.id, #inheritence from application controller
+            product_id: params[:product_id],
+            quantity: params[:quantity],
+            subtotal: calculated_subtotal,
+            tax: calculated_tax,
+            total: calculated_total
+            )
+        order.save
+        render json: order.as_json
     end
+
+    def show
+        order = current_user.orders.find_by(id: params[:id])
+        render json: order
+    end
+
+    def index
+        orders = current_user.orders
+        render json: orders
+    end
+
 end
