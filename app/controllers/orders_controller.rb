688a2d761552afa8956b959db9a691608841e3a1
Alex <alexgmiller00@gmail.com>
2022-03-28 12:32:02 -0700

    Adds order update_totals

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,19 @@ class OrdersController < ApplicationController
     before_action :authenticate_user
 
     def create
-        product = Product.find_by(id: params[:product_id])
-        calclated_subtotal = product.price * params[:quantity]
-        calculated_tax = calculated_subtotal * 0.09
-        calculated_total = calculated_subtotal + calculated_tax
+        # product = Product.find_by(id: params[:product_id])
+        # calclated_subtotal = product.price * params[:quantity]
+        # calculated_tax = calculated_subtotal * 0.09
+        # calculated_total = calculated_subtotal + calculated_tax
 
         order = Order.new(
-            user_id: current_user.id, #inheritence from application controller
-            # product_id: params[:product_id],
-            # quantity: params[:quantity],
-            subtotal: calculated_subtotal,
-            tax: calculated_tax,
-            total: calculated_total
+            user_id: current_user.id, 
             )
         order.save
-        render json: order.as_json
+        carted_products = current_user.carted_products.where(status: "carted")
+        carted_products.update_all(status: "purchased", order_id: @order.id)
+        order.update_totals
+        render template: "orders/show"
     end
 
     def show
